e2902d89aa0e4bc5d833deb2f87ab0a89eabb209
washucode <estherw8525@gmail.com>
2019-10-11 12:53:34 +0300

    create signup form with extra fields using UserCreationFrom in forms.py

---
+++ main/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class RegisterForm(UserCreationForm):
+    firstname = forms.CharField(max_length=30,required=False,help_text='Optional.')
+    surname = forms.CharField(max_length=30,required=False,help_text='Optional.')
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    class Meta :
+        model = User
+        fields =('username','firstname','surname','email','password','confirm_password')
--- main/views.py
+++ main/views.py
@@ -1,7 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
-# Create your views here.
-def login(request):
-    return HttpResponse('Login')
+from django.contrib.auth import login, authenticate
 
