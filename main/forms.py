9c620ab6b01edaa1b0497b4fcfeccbae3a9833af
washucode <estherw8525@gmail.com>
2019-10-11 13:11:17 +0300

    add code in views to validate and register user

--- main/forms.py
+++ main/forms.py
@@ -9,4 +9,4 @@ class RegisterForm(UserCreationForm):
     email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
     class Meta :
         model = User
-        fields =('username','firstname','surname','email','password','confirm_password')
+        fields =('username','firstname','surname','email','password1','password2')
--- main/views.py
+++ main/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
 from django.contrib.auth import login, authenticate
+from forms import RegisterForm
 
+
+def signup(request):
+
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            input_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username,password=input_password)
+            login(request, user)
+            return render(request, 'index.html')
+    else :
+        form = UserCreationForm()
+    return render(request, 'auth/signup.html', {'form':form})
+            
