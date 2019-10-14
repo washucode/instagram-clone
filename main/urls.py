bcbe413de6a94055fcf280d681de737becc8a7c8
washucode <estherw8525@gmail.com>
2019-10-14 05:18:48 +0300

    make profile page to display details of the account owner

--- main/urls.py
+++ main/urls.py
@@ -2,11 +2,12 @@ from django.urls import include, path
 from . import views
 from django.contrib.auth import views as auth_views
 
+
 urlpatterns =[
     path('', views.signup, name='signup'),
     path('home/', views.home, name = 'home'),
     path('login/', auth_views.LoginView.as_view(template_name = 'auth/login.html'), name = 'login'),
-    path('logout/', auth_views.LogoutView.as_view(), name = 'logout')
-    path('profile/', view.profile , name = 'profile')
+    path('logout/', auth_views.LogoutView.as_view(), name = 'logout'),
+    path('profile/', views.profile , name = 'profile')
 
 ]
