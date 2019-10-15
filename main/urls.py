1c3f906d5b4e9839e442e6b488744eb634e85241
washucode <estherw8525@gmail.com>
2019-10-15 12:18:46 +0300

    add logout functionality to profile

--- main/urls.py
+++ main/urls.py
@@ -8,7 +8,7 @@ urlpatterns =[
     path('', views.signup, name='signup'),
     path('home/', views.home, name = 'home'),
     path('login/', auth_views.LoginView.as_view(template_name = 'auth/login.html'), name = 'login'),
-    path('logout/', auth_views.LogoutView.as_view(), name = 'logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='auth/thankyou.html'), name = 'logout'),
     path('profile/', views.profile , name = 'my_profile'),
     path('image/new', views.createimage.as_view(), name = 'post'),
     re_path(r'^comment/(?P<image_id>\d+)$',views.comments,name='comments'),
